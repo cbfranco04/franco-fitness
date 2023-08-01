7ed846cedd1eeb6ee9181301d6e00cfd0c2d90fb
ABFranco <abfranco4@gmail.com>
2023-08-01 06:04:58 -0700

    add router and initial homepage

---
+++ src/components/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Fitness app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
